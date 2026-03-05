bf75e4e62035994a8eb9f5625b3a3dead6386379
𝔻𝕠𝕠𝕞𝕫𝕖𝕖 <108785158+SkyNinjaX@users.noreply.github.com>
2026-03-05 18:17:39 +0100

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,17 +1,15 @@
 import firebase_admin
 from firebase_admin import credentials, auth, firestore
-import os
 import json
+import os
 
-# Try to read from environment variable first (for Render)
+# Read credentials from environment variable
 cred_json = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
-
 if cred_json:
-    # Running on Render - read from env variable
     cred_dict = json.loads(cred_json)
     cred = credentials.Certificate(cred_dict)
 else:
-    # Running locally - read from file
+    # Fallback to local file for development
     current_dir = os.path.dirname(os.path.abspath(__file__))
     key_path = os.path.join(current_dir, "serviceAccountKey.json")
     cred = credentials.Certificate(key_path)
